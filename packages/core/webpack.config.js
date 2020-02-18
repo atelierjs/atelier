434b98ee69249e2e0c2865b50a6f28f3870b7c7d
Travis Henning <henningta@gmail.com>
2020-02-18 15:44:07 -0500

    Moved scss stuff into 'core' package

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,5 +1,5 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('../../webpack.common.js');
 
 // const isDev = process.env.NODE_ENV === 'development';
 
